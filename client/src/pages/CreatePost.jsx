46b143693baf253ed5116b35a0da077b435c0338
inmanolov <you@example.com>
2023-01-24 13:41:21 +0200

    initial backend added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,8 +15,33 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const generateImage = () => {
+    const generateImage = async () => {
+        if (form.prompt) {
+            try {
+                setGeneratingImg(true)
 
+                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        prompt: form.prompt,
+                    }),
+                });
+
+                const data = await response.json();
+
+                setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
+            } catch (error) {
+                alert(error);
+                console.log(error)
+            } finally {
+                setGeneratingImg(false);
+            }
+        } else {
+            alert('Propmt is required')
+        }
     }
 
     const handleSubmit = () => {
@@ -32,16 +57,16 @@ const CreatePost = () => {
         setForm({ ...form, prompt: randomPrompt });
     }
 
-    return(
+    return (
         <section className='max-w-7xl mx-auto'>
-           <div>
+            <div>
                 <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
                 <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]' >Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
             </div>
-            
+
             <form onSubmit={handleSubmit} className='mt-16 max-w-3xl'>
                 <div className='flex flex-col gap-5'>
-                    <FormField 
+                    <FormField
                         labelName="Your Name"
                         text="text"
                         name="name"
@@ -49,7 +74,7 @@ const CreatePost = () => {
                         value={form.name}
                         handleChange={handleChange}
                     />
-                    <FormField 
+                    <FormField
                         labelName="Prompt"
                         text="text"
                         name="prompt"
@@ -61,13 +86,13 @@ const CreatePost = () => {
                     />
                     <div className='relative bg-gay-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 h-64 p-3 flex justify-center items-center'>
                         {form.photo ? (
-                            <img 
+                            <img
                                 src={form.photo}
                                 alt={form.prompt}
                                 className='w-full h-full obcjecet-contain'
                             />
                         ) : (
-                            <img 
+                            <img
                                 src={preview}
                                 alt="preview"
                                 className='w-9/12 h-9/12 object-contain opacity-40'
@@ -88,7 +113,7 @@ const CreatePost = () => {
                         onClick={generateImage}
                         className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
                     >
-                        {generatingImg ? 'Generating...' : 'Generate' }
+                        {generatingImg ? 'Generating...' : 'Generate'}
                     </button>
                 </div>
                 <div className='mt-10'>
